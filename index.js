54402a99e6e4a5832fb6863ebad332ec843b52ea
Ricardo Hernandez <ricardohs35@gmail.com>
2020-12-04 12:26:13 -0600

    :sparkles: Add basic form for video submit

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const electron = require('electron');
-const { app } = electron;
+const { app, BrowserWindow, ipcMain } = electron;
 
 app.on('ready', () => {
-  console.log('Electron App is ready');
+  const mainWindow = new BrowserWindow({
+    webPreferences: { nodeIntegration: true }
+  });
+
+  mainWindow.loadURL(`file://${__dirname}/index.html`);
+});
+
+ipcMain.on('video:submit', (event, filePath) => {
+  console.log('Video submitted', filePath);
 });
